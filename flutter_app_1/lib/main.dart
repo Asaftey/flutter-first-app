df940d687f74ecc95d7ef731a54833fa81cd1724
Ionut Asaftei <ionut.asaftei2014@gmail.com>
2022-12-05 23:21:12 +0100

    second commit inizia app

--- flutter_app_1/lib/main.dart
+++ flutter_app_1/lib/main.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp() => MyApp;
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("hello ivana"),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My first app!'),
+          ),
+          body: Column(
+            children: [
+              Text('The question!'),
+              ElevatedButton(
+                onPressed: null,
+                child: Text('Ansewer 1'),
+              ),
+              ElevatedButton(
+                onPressed: null,
+                child: Text('Ansewer 2'),
+              ),
+              ElevatedButton(
+                onPressed: null,
+                child: Text('Ansewer 3'),
+              ),
+            ],
+          )),
     );
   }
 }
